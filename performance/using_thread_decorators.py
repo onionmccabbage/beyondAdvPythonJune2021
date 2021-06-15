e7528df780511319b43a98168e255ff6b7582053
onionmccabbage <onionmccabbage@live.com>
2021-06-15 13:57:09 +0100

    Add files via upload

--- performance/using_thread_decorators.py
+++ performance/using_thread_decorators.py
@@ -1,5 +1,5 @@
 # we can implement threads by writing functions then decorating other functions
-
+from threading import Lock
 # here are some functions which will be used as decorators
 
 def lock_class(methodnames, lockfactory):
@@ -17,19 +17,37 @@ def lock_method(method):
         return locked_method
 
 
-def make_thread_safe():
-    pass
+def make_thread_safe(cls, methodnames, lockfactory):
+    init = cls.__init__
+    def new_init(self, *args, **kwargs):
+        init(self, *args, **kwargs)
+        self.__lock = lockfactory
+    cls.__init__ = new_init
+    # iterate over the methods of this class, making the mthread safe
+    for methodname in methodnames:
+        oldmethod = getattr(cls, methodname)
+        newmethod = lock_method(oldmethod)
+        setattr(cls, methodname, newmethod)
+        print('method {} is locked'.format(methodname))
+    return cls
 
-@lock_class # decorate this class using lock_class function
-class ClassDecoratedLockedSet(set):
-    pass
-
+# the decorator needs to pass the lock methods and the lock (provides the lockfactory)
+@lock_class(['add', 'remove'], Lock) # decorate this class using lock_class function
+class ClassDecoratedLockedSet(set): # nb this class does NOT descend from Thread
+    @lock_method
+    def methodToLock(self): # nb the methods are not themselves threadsafe - only due to the decorator
+        print('this method will be locked (and therefore thread-safe)')
+    @lock_method
+    def otherMethodToLock(self):
+        print('this other method will also be locked')
 def main():
     my_set = (4,3,2)
     my_inst = ClassDecoratedLockedSet(my_set)
     # is it locked?
     print('Is this locked: {}'.format(my_inst.add.__is_locked))
-    # print(my_inst)
+    print(my_inst.add.__is_locked)
+    print(my_inst.methodToLock.__is_locked)
+    print(my_inst.otherMethodToLock.__is_locked)
 
 if __name__ == '__main__':
     main()
